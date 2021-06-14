7fd048fcc15a967d97bf9f9ed6dd92e6eb9ba43b
Glitch (nonstop-verdant-property) <none>
2021-06-14 08:41:22 +0000

    🖇🚲 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# style-dictionary-severless
+# 🎨 Sytle Dictionary | Commit Design Tokens To Github
+
+![style-dictionary](https://pbs.twimg.com/media/EswGlXQXAAA5prH?format=jpg&name=small)
+
+A Glitch serverless function for committing design tokens from the [Design Tokens plugin for Figma](https://github.com/lukasoppermann/design-tokens) to my [style-dictionary repository](https://github.com/michaelmang/style-dictionary).
+
+This is part of a series of tutorials for my design tokens ebook that is in the work: [https://www.michaelmang.dev/blog/exporting-design-tokens-from-figma-with-style-dictionary](https://www.michaelmang.dev/blog/exporting-design-tokens-from-figma-with-style-dictionary)
---
+++ server.js
@@ -0,0 +1,52 @@
+const { Octokit } = require("@octokit/core");
+
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.text());
+
+const octokit = new Octokit({ auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.post("/commit-tokens", async (req, res) => {
+  let file;
+
+  try {
+    file = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+      owner: "ayoola-solomon",
+      repo: "style-dictionary",
+      path: "input/design-tokens.json"
+    });
+  } catch (ex) {
+    // swallow 404
+  }
+
+  const body = JSON.parse(req.body);
+  const buffer = Buffer.from(body.client_payload.tokens);
+  const content = buffer.toString("base64");
+
+  await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+    owner: "ayoola-solomon",
+    repo: "style-dictionary",
+    path: "input/design-tokens.json",
+    message: "Update design tokens",
+    content,
+    sha: file && file.data && file.data.sha ? file.data.sha : null
+  });
+
+  res.sendStatus(200);
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
